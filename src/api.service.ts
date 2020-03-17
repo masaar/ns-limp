a421c63387e23e3ef9979fa907ff76e174af9cef
Ali <ali.q@masaar.com>
2020-03-17 09:15:37 +0500

    -version 6.1.0-dev

--- src/api.service.ts
+++ src/api.service.ts
@@ -20,7 +20,7 @@ export interface SDKConfig {
 	authAttrs: Array<string>;
 	debug?: boolean;
 	fileChunkSize?: number;
-	authHashLevel?: 5.0 | 5.6;
+	authHashLevel?: 5.0 | 5.6 | 6.1;
 }
 
 export interface QueryStep {
@@ -140,7 +140,7 @@ export class ApiService {
 		authAttrs: [],
 		debug: false,
 		fileChunkSize: 500 * 1024,
-		authHashLevel: 5.6
+		authHashLevel: 6.1
 	}
 
 	private subject!: Subject<any>;
@@ -212,7 +212,7 @@ export class ApiService {
 
 		this.authed$.subscribe((session) => {
 			if (session) {
-				if (this.queue.noAuth) {
+				if (this.queue.auth) {
 					this.log('info', 'Found calls in auth queue:', this.queue.auth);
 				}
 				for (let call of this.queue.auth) {
@@ -486,21 +486,43 @@ export class ApiService {
 		return call;
 	}
 	generateAuthHash(authVar: string, authVal: string, password: string): string {
-		if (this.config.authAttrs.indexOf(authVar) == -1) {
-			throw new Error(`Unkown authVar '${authVar}'. Accepted authAttrs: '${this.config.authAttrs.join(', ')}'`);
+
+		if (this.config.authAttrs.indexOf(authVar) == -1 && authVar != 'token') {
+			throw new Error(`Unkown authVar '${authVar}'. Accepted authAttrs: '${this.config.authAttrs.join(', ')}, token'`)
+
+		// if (this.config.authAttrs.indexOf(authVar) == -1) {
+		// 	throw new Error(`Unkown authVar '${authVar}'. Accepted authAttrs: '${this.config.authAttrs.join(', ')}'`);
 		}
-		let oHeader = { alg: 'HS256', typ: 'JWT' };
-		let sHeader = JSON.stringify(oHeader);
-		let hashObj = [authVar, authVal, password];
-		if (this.config.authHashLevel == 5.6) {
-			hashObj.push(this.config.anonToken);
+		// let oHeader = { alg: 'HS256', typ: 'JWT' };
+		// let sHeader = JSON.stringify(oHeader);
+		// let hashObj = [authVar, authVal, password];
+		// if (this.config.authHashLevel == 5.6) {
+		// 	hashObj.push(this.config.anonToken);
+
+		if (this.config.authHashLevel != 6.1) {
+			let oHeader = { alg: 'HS256', typ: 'JWT' };
+			let sHeader = JSON.stringify(oHeader);
+			let hashObj = [authVar, authVal, password];
+			if (this.config.authHashLevel == 5.6) {
+				hashObj.push(this.config.anonToken);
+			}
+			let sPayload = JSON.stringify({ hash: hashObj });
+			let sJWT = JWS.sign('HS256', sHeader, sPayload, { utf8: password });
+			return sJWT.split('.')[1];
+		} else {
+			if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/)) {
+				throw new Error('Password should be 8 chars, contains one lower-case char, one upper-case char, one number at least.');
+			}
+			return `${authVar}${authVal}${password}${this.config.anonToken}`;
 		}
-		let sPayload = JSON.stringify({ hash: hashObj });
-		let sJWT = JWS.sign('HS256', sHeader, sPayload, { utf8: password });
-		return sJWT.split('.')[1];
+		// let sPayload = JSON.stringify({ hash: hashObj });
+		// let sJWT = JWS.sign('HS256', sHeader, sPayload, { utf8: password });
+		// return sJWT.split('.')[1];
 	}
 
 	auth(authVar: string, authVal: string, password: string): Observable<Res<Doc>> {
+
+		if (this.authed) throw new Error('User already authed.');
 		if (this.config.authAttrs.indexOf(authVar) == -1) {
 			throw new Error(`Unkown authVar '${authVar}'. Accepted authAttrs: '${this.config.authAttrs.join(', ')}'`);
 		}
@@ -522,7 +544,8 @@ export class ApiService {
 			sid: 'f00000000000000000000012',
 			token: this.config.anonToken,
 			query: [
-				{ _id: sid || 'f00000000000000000000012', hash: sJWT.split('.')[1] }
+				// { _id: sid || 'f00000000000000000000012', hash: sJWT.split('.')[1] }
+				{ _id: sid || 'f00000000000000000000012', token: token }
 			]
 		});
 		call.subscribe({
@@ -541,6 +564,7 @@ export class ApiService {
 	}
 
 	signout(): Observable<Res<Doc>> {
+		if (!this.authed) throw new Error('User not authed.');
 		let call = this.call('session/signout', {
 			query: [
 				{ _id: this.cache.get('sid') }
